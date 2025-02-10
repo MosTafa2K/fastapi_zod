a8061beadfb2e072925c078ef00d78f2ce648c8c
mostafa2k <m2kappswindows8.1@gmail.com>
2025-02-10 10:18:23 +0330

    Initial project(backend)

---
+++ backend/data.py
@@ -0,0 +1,36 @@
+### Simulate database data
+backend_data = [
+    {
+        "id": 1,
+        "title": "Neuromancer",
+        "year": 1984,
+        "edition": 1,
+        "author": {
+            "id": 1,
+            "name": "William Gibson",
+            "bio": "William Gibson is a Canadian novelist, short story writer, screenwriter, and film director.",
+        },
+    },
+    {
+        "id": 2,
+        "title": "The Martian",
+        "year": 2011,
+        "edition": 1,
+        "author": {
+            "id": 2,
+            "name": "Andy Weir",
+            "bio": "Andy Weir is a Canadian novelist, short story writer, screenwriter, and film director.",
+        },
+    },
+    {
+        "id": 3,
+        "title": "The Martian",
+        "year": 2011,
+        "edition": 2,
+        "author": {
+            "id": 2,
+            "name": "Andy Weir",
+            "bio": "Andy Weir is a Canadian novelist, short story writer, screenwriter, and film director.",
+        },
+    },
+]
---
+++ backend/main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from .data import backend_data
+
+
+app = FastAPI()
