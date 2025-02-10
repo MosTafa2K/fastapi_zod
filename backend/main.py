b7880b3c13c379d7bc041b048517f424d9161f8c
mostafa2k <m2kappswindows8.1@gmail.com>
2025-02-10 11:54:27 +0330

    feat(backend): add endpoints for retrieving books list and book details

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
-from .data import backend_data
+from data import backend_data as books
+from models import Author, Book
 
 
 app = FastAPI()
+
+
+@app.get("/api/books", response_model=list[Book])
+async def get_books_list():
+    return books
+
+
+@app.get("/api/books/{book_id}", response_model=Book)
+async def get_book_by_id(book_id: int):
+    book = books[book_id - 1]
+    return book
