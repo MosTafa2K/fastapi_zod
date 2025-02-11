6932c9a2fc8452af3427d7f1e6460101285f5d3e
mostafa2k <m2kappswindows8.1@gmail.com>
2025-02-11 09:46:57 +0330

    feat(backend): add CORS middleware to allow frontend access

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from data import backend_data as books
 from models import Author, Book
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/books", response_model=list[Book])
